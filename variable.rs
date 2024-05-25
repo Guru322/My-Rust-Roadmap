3f4bf6a62453bd49238b315053671a6e7ba27e82
GURU SENSEI <sahoogurucharan00@gmail.com>
2024-05-25 18:55:15 +0530

    Create variable.rs

---
+++ variable.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let _x = 90; //immutable
+    println!("x");
+    let mut a = 5;  //mutable
+    println!("The value of a is {}", a );
+    a = 10;
+    println!("The new value of a is {}", a);
+    let mut b = "i am guru";
+    println!("{}", b );
+    b = "guru now GURU";
+    print!("{}",b) //println! just adds a new line
+}
